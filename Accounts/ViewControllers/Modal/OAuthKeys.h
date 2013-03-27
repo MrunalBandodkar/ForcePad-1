df0d5f0167a867c6788ff84bd35d63fc32de47c9
Mrunal Bandodkar <mrunalbandodkar@Mrunals-MacBook-Pro.local>
2013-03-27 14:33:28 -0700

    Added OAuth header file

---
+++ Accounts/ViewControllers/Modal/OAuthKeys.h
@@ -0,0 +1,16 @@
+//
+//  OAuthKeys.h
+//  ForcePad
+//
+//  Created by Mark Prichard on 3/6/13.
+//
+//
+
+#ifndef ForcePad_OAuthKeys_h
+#define ForcePad_OAuthKeys_h
+
+#define OAuthClientID               @"3MVG9A2kN3Bn17huPEetajQs5DHYBKtHn42N9zQmCQqC3oeyMkMefdapRFwOeckSx_qXrTIUU0as0NJW3cm6I"
+#define OAuthConsumerSecret         @"1431983421291794239"
+
+
+#endif
